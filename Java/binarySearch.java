3c845e13301cb98f91d9d59b84057d66a2edcb55
iamspidysa <iamspidysa@users.noreply.github.com>
2024-07-16 13:04:27 +0530

    New File Binary Search

---
+++ Java/binarySearch.java
@@ -0,0 +1,34 @@
+package Java;
+
+public class binarySearch {
+    public static void binarySearching(int arr[], int target){
+
+        int lo = 0;
+        int hi = arr.length-1;
+        int index = -1;
+        while(lo<=hi){
+            int mid = (lo+hi)/2;
+            if(arr[mid]==target){
+                index = mid;
+                break;
+            }
+            else if(arr[mid]>target){
+                hi = mid -1;
+            }
+            else{
+                lo = mid +1;
+            }
+        }
+        if(index > 0){
+            System.out.println("target Element Fount at "+index);
+        }
+        else{
+            System.out.println("Not Found");
+        }
+    }
+    public static void main(String[] args) {
+        int array[] = {1,2,4,5,7,9,10,45,50,60};
+        int target = 50;
+        binarySearching(array,target);
+    }
+}
